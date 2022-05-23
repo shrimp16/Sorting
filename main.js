c8060544bd760e748461d30f225364fbff0078e3
Shrimp <lcamara0916@gmail.com>
2022-05-23 13:27:37 +0000

    Deleted unnecessary stuff

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const initialFile = yargs.argv._[0];
 const sortedFile = yargs.argv._[1];
 const sortBy = yargs.argv._[2];
-
+ 
 const data = JSON.parse(fs.readFileSync(`${initialFile}.json`));
 
 console.log(data);
@@ -24,15 +24,3 @@ const newData = data.sort((a, b) => {
 fs.writeFile(`${sortedFile}.json`, JSON.stringify(newData, null, 2), (err) => {
     if(err) throw err;
 })
-
-//console.log(names.sort());
-
-/*console.log(data.sort((a, b) => {
-    if (a[sortBy] > b[sortBy]) {
-        return 1;
-    }
-    if (a[sortBy] < b[sortBy]) {
-        return -1;
-    }
-    return 0;
-}))*/
