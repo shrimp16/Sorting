f130762e95c4ff48e316f98167a07eee27078434
Shrimp <lcamara0916@gmail.com>
2022-05-23 13:22:00 +0000

    Deleted old obj

--- main.js
+++ main.js
@@ -13,52 +13,6 @@ fs.writeFile(`${sortedFile}.json`, JSON.stringify(data, null, 2), (err) => {
     if(err) throw err;
 })
 
-const names = [
-    "Luis",
-    "Beatriz",
-    "Date a Live I",
-    "Bruno",
-    "Bruna",
-    "Sofia",
-    "Date a Live II",
-    "Luis Camara",
-    "Date a Live III",
-    "Luis Antonio",
-    "Date a Live IV",
-    "Date a Live A"
-]
-
-const obj = [
-    {
-        name: "Luis",
-        age: 18
-    },
-    {
-        name: "Sofia",
-        age: 17
-    },
-    {
-        name: "Beatriz",
-        age: 15
-    },
-    {
-        name: "Bruno",
-        age: 19
-    },
-    {
-        name: "Bruna",
-        age: 12
-    },
-    {
-        name: "John",
-        age: 12
-    },
-    {
-        name: "Andre",
-        age: 12
-    },
-]
-
 //console.log(names.sort());
 
 /*console.log(data.sort((a, b) => {
